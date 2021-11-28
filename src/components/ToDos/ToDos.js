3dc73b862db0834a78afa0350612a11c1179621d
Satuev <alitm.s@mail.ru>
2021-11-28 20:27:57 +0300

    добавил карточку

--- src/components/ToDos/ToDos.js
+++ src/components/ToDos/ToDos.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from "react";
-import ToDo from "./ToDo";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCategories, fetchTodos } from "../../redux/features/ToDo";
+import React, { useEffect } from "react"
+import ToDo from "./ToDo"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchCategories, fetchTodos } from "../../redux/features/ToDo"
 
 const ToDos = () => {
-  const dispatch = useDispatch();
-  const category = useSelector((state) => state.categories);
+  const dispatch = useDispatch()
+  const category = useSelector((state) => state.categories)
 
   useEffect(() => {
-    dispatch(fetchTodos()); //вывод тудушек
-  }, []);
+    dispatch(fetchTodos()) //вывод тудушек
+  }, [])
 
   useEffect(() => {
-    dispatch(fetchCategories()); //вывод категорий
-  }, []);
+    dispatch(fetchCategories()) //вывод категорий
+  }, [])
   return (
-    <div style={{display: "flex"}}>
+    <div style={{ display: "flex" }}>
       {category.map((todo) => {
         return (
           <div className="row">
             <div className="col border-top-5">
-              <div className="card "  style={{minHeight: "300px"}}>
+              <div className="card " style={{ minHeight: "300px" }}>
                 <h5 className="card-header text-center">{todo.name}</h5>
                 <div className="card-body">
-                  {/*<ToDo id={todo._id}/>*/}
+                  <ToDo id={todo._id} />
                 </div>
               </div>
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 //Здесь должны быть столбцы из категорий с их тудушками
 
 //прописать useEffect
-export default ToDos;
+export default ToDos
